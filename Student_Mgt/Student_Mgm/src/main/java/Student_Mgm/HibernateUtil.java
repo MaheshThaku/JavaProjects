5c076c5746f66b32c1abd5ffceb8af3957542e15
Mahesh Thakur <69119951+MaheshThaku@users.noreply.github.com>
2022-10-28 10:18:29 +0530

    Student_Mgm
    @ developed by Mahesh Thakur

---
+++ Student_Mgt/Student_Mgm/src/main/java/Student_Mgm/HibernateUtil.java
@@ -0,0 +1,17 @@
+// @ Developed by Mahesh Thakur
+package Student_Mgm;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+//for configuration of database
+public class HibernateUtil {
+
+	public static Session getSeesion() {
+		Configuration con = new Configuration().configure().addAnnotatedClass(Student.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session session = sf.openSession();
+		return session;
+	}
+}
